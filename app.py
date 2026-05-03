e6d5198b259c34c10529484c43b243bd519d0fe5
kaharnasyrhan <nwkahar08@mail.com>
2026-05-03 18:51:53 +0500

    implement user registration, login and session logic for Week 2

--- app.py
+++ app.py
@@ -1,10 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import json
+import os
+from decorators import login_required
 
 app = Flask(__name__)
+app.secret_key = "super_secret_key"  # Необходимо для работы сессий
+DATA_FILE = 'data/users.json'
 
-@app.route("/")
+
+def load_users():
+    if not os.path.exists(DATA_FILE):
+        return {}
+    with open(DATA_FILE, 'r') as f:
+        return json.load(f)
+
+
+def save_users(users):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(users, f, indent=4)
+
+
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-if __name__ == "__main__":
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        full_name = request.form['full_name']
+
+        users = load_users()
+        if username in users:
+            return "User already exists!"
+
+        users[username] = {"password": password, "full_name": full_name}
+        save_users(users)
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        users = load_users()
+        if username in users and users[username]['password'] == password:
+            session['user'] = username
+            return redirect(url_for('dashboard'))
+        return "Invalid credentials"
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=session['user'])
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
     app.run(debug=True)
